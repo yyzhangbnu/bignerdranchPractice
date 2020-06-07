c5524c8230a88ee4feb360394d2675273676cdc4
yaozha <yaozha@microsoft..com>
2020-06-07 11:33:10 +0800

    Fragment argument

---
+++ CriminalIntent_09/app/src/main/java/com/example/criminalintent/CrimeActivity.java
@@ -0,0 +1,30 @@
+package com.example.criminalintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.UUID;
+
+public class CrimeActivity extends SingleFragmentActivity {
+    public static final String EXTRA_CRIME_ID = "com.bignerdranch.android.criminalintent.crime";
+
+    @Override
+    protected Fragment createFragment() {
+       // return new CrimeFragment();
+
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+        return CrimeFragment.newInstance(crimeId);
+    }
+
+    public static Intent newIntent(Context packageContext, UUID crimeId){
+        Intent intent = new Intent(packageContext, CrimeActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+
+        return intent;
+    }
+}
---
+++ CriminalIntent_09/app/src/main/java/com/example/criminalintent/SingleFragmentActivity.java
@@ -0,0 +1,31 @@
+package com.example.criminalintent;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    // abstract class must have definition at children class
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        // get fragment manager
+        FragmentManager fm = getSupportFragmentManager();
+
+        // use the fragment manager to find the fragment
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        // if fragment is null, try to set one and transaction and commit
+        if(fragment == null){
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+    }
+}
